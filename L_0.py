04ad39cf4b89d56cf50182f27e23ce65afb1d5e6
Pankaj Yadav <pankajyadav2021ii@gmail.com>
2025-07-16 17:41:01 +0530

    Add files via upload
    Signed-off-by: Pankaj Yadav <pankajyadav2021ii@gmail.com>

---
+++ L_0.py
@@ -0,0 +1,6 @@
+# We Know fundamentantals of loops so we will practice as much as we can and make things note down which are important to grab.
+# Here again be attentive about the indentataion and scope of conditions and loops scope and .  
+# If print statement executed outside the scope of loop it will print last loops executed value ! i guess you got it ! 
+
+
+# Quick fact : do you know why the printed values in for loop executes in diffrenet lines because in scope of for loop print statement has bydefault end = "\n" thats why it happens but if we put  other thing it will get in such way.
